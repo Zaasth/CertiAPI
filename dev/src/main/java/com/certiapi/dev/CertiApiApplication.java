f65b17f3554458b57dc83b9b9523197102d048dd
Zaasth <wellintonpaypal@gmail.com>
2025-07-28 14:24:59 -0300

    innitial commit

---
+++ dev/src/main/java/com/certiapi/dev/CertiApiApplication.java
@@ -0,0 +1,13 @@
+package com.certiapi.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertiApiApplication.class, args);
+	}
+
+}
---
+++ dev/src/test/java/com/certiapi/dev/CertiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certiapi.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
